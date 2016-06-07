83a0995349f1532a4b4cda520c06f6a71ba24d75
HavikIV <manjinder360@yahoo.com>
2016-06-07 12:55:45 -0700

    Cleaned up some of the code in the candies class.

--- CandyCrush/CandyCrush/candies.cs
+++ CandyCrush/CandyCrush/candies.cs
@@ -12,7 +12,7 @@ namespace CandyCrush
     // it will contain private variables for it's Name, location, and whether or not it's a special candy
     public class candy
     {
-        private string name, stripeDirection;
+        private string candyType, stripeDirection;
         private Random rand;
         // As I've decided to added room for striped candies, I no longer need a bool variable to check if it's striped.
         // As I'm longer going to create an ImageView in this class, there's no need for variables for X and Y locations.
@@ -21,60 +21,16 @@ namespace CandyCrush
 
         // default constructor
         // generates a random candy
-        public candy(Random r)
+        public candy(Random r, string sDirection)
         {
             rand = r;
+            //string[] candyTypes = { "Peppermint Swirl", "Blue Jolly Rancher", "Candy Corn", "Purple Nerd", "Green Elliptical" };
+            string[] candyTypes = { "red", "blue", "yellow", "purple", "green" };
             int candyNumber = generateCandy(); // gets the candy number
-            stripeDirection = "None"; // by default the candy has no stripeDirection, only if it's a special candy
-            //  determine what candy it is based on the candyNumber and assign it's name
-            if (candyNumber == 1)
-            {
-                name = "Peppermint Swirl";
-
-            }
-            else if (candyNumber == 2)
-            {
-                name = "Blue Jolly Rancher";
-
-            }
-            else if (candyNumber == 3)
-            {
-                name = "Candy Corn";
-            }
-            else if (candyNumber == 4)
-            {
-                name = "Purple Nerd";
-            }
-            else if (candyNumber == 5)
-            {
-                name = "Green Elliptical";
-            }
-            else if (candyNumber == 6)
-            {
-                name = "Striped Peppermint Swirl";
-            }
-            else if (candyNumber == 7)
-            {
-                name = "Striped Blue Jolly Rancher";
-            }
-            else if (candyNumber == 8)
-            {
-                name = "Striped Candy Corn";
-            }
-            else if (candyNumber == 9)
-            {
-                name = "Striped Purple Nerd";
-            }
-            else if (candyNumber == 10)
-            {
-                name = "Striped Green Elliptical";
-            }
-
-            //  assign stripeDirection
-            if (candyNumber > 5 && candyNumber <= 10)
-            {
-                stripeDirection = generateDirecction();
-            }
+            stripeDirection = sDirection; // by default the candy has no stripeDirection, only if it's a special candy
+            //  assign what candy it is based on the candyNumber
+            candyType = candyTypes[candyNumber];
+            stripeDirection = sDirection;   // Assigns the direction of the stripes
         }
 
         // Generates a candy by generating a random number 
@@ -84,9 +40,9 @@ namespace CandyCrush
         }
 
         //  Returns the candy's name
-        public string getName()
+        public string getType()
         {
-            return name;
+            return candyType;
         }
 
         //  Returns the candy's stripe direction
@@ -95,30 +51,15 @@ namespace CandyCrush
             return stripeDirection;
         }
 
-        //  returns a direction for stripes on the candy
-        public string generateDirecction()
-        {
-            Random randDirection = new Random(); // declares a random object
-            int randNumber = randDirection.Next(1, 2); // assigns either 1 or 2 to randNumber
-           if (randNumber == 1)
-            {
-                return "Horizontal";
-            }
-           else
-            {
-                return "Vertical";
-            }
-        }
-
         // Should have a way to create a special candy for when one of several certain combinations are met
         // create the function here
         public void generateSpecial(string candy, string direction)
         {
-            name = candy;
+            candyType = candy;
             stripeDirection = direction;
         }
 
-        // Generate a random number within the range of 1-1000
+        // Generate a random number within the range of 1-750
         // will return a number that corresponds with a candy after the random number is evaluated
         // the first 500 numbers will used to candies of being red, blue, yellow, purple, and green
         // 1-150 -> Red, 151-300 -> Blue, 301-450 -> Yellow, 451-600 -> Purple, 601-750 -> Green
@@ -129,62 +70,35 @@ namespace CandyCrush
         {
             //Random rand = new Random(); // declares a random object
             int randomNumber; // variable to hold a random number
-            randomNumber = rand.Next(1, 750); // assigns a number between 1-500 to randomNumber
+            randomNumber = rand.Next(1, 750); // assigns a number between 1-750 to randomNumber
 
             // determine what candy is generated
             if (randomNumber <= 150)
             {
                 // Red candy is generated
-                return 1;
+                return 0;
             }
             else if (randomNumber > 151 && randomNumber <= 300)
             {
                 // Blue candy is generated
-                return 2;
+                return 1;
             }
             else if (randomNumber > 301 && randomNumber <= 450)
             {
                 // Yellow candy is generated
-                return 3;
+                return 2;
             }
             else if (randomNumber > 450 && randomNumber <= 600)
             {
                 // Purple candy is generated
-                return 4;
+                return 3;
             }
             else if (randomNumber > 600 && randomNumber <= 750)
             {
                 // Green candy is generated
-                return 5;
+                return 4;
             }
-            else if (randomNumber > 751 && randomNumber <= 780)
-            {
-                // striped red candy
-                return 6;
-            }
-            else if (randomNumber > 781 && randomNumber <= 810)
-            {
-                // striped blue candy
-                return 7;
-            }
-            else if (randomNumber > 811 && randomNumber <= 840)
-            {
-                // striped yellow candy
-                return 8;
-            }
-            else if (randomNumber > 841 && randomNumber <= 870)
-            {
-                // striped purple candy
-                return 9;
-            }
-            else if (randomNumber > 871 && randomNumber <= 900)
-            {
-                // striped green candy
-                return 10;
-            }
-            // add the other candies here
-            // the last 10% of the numbers should be used to create other special candies
-            return 1; // returns a red candy by default
+            return 0; // returns a red candy by default
         }
     }
 }
--- CandyCrush/CandyCrush/candyGrid.cs
+++ CandyCrush/CandyCrush/candyGrid.cs
@@ -38,13 +38,13 @@ namespace CandyCrush
 
         public void assignCandy(int row, int col)
         {
-            grid[row, col] = new candy(rand);
+            grid[row, col] = new candy(rand, "none"); // candies should have no stripes when the level is loaded
         }
 
         //  Displays the candy at the specific location in the  grid
         public string displayCandy(int row, int col)
         {
-            return grid[row, col].getName();
+            return grid[row, col].getType();
         }
 
         //  Displays all of the candies in the grid recursively from the first candy to the last, row-wise
@@ -52,13 +52,13 @@ namespace CandyCrush
         {
             if (col == 8 && row != 8)
             {
-                return " " + grid[row, col].getName() + displaygrid(row + 1, 0) + "\n";
+                return " " + grid[row, col].getType() + displaygrid(row + 1, 0);
             }
             else if (col != 8)
             {
-                return " " + grid[row, col].getName() + displaygrid(row, col + 1);
+                return " " + grid[row, col].getType() + displaygrid(row, col + 1);
             }
-            return " " + grid[row, col].getName();
+            return " " + grid[row, col].getType();
         }
 
         public void findCombos()
@@ -68,22 +68,22 @@ namespace CandyCrush
             {
                 for (int col = 0; col < 7; col++)
                 {
-                    if (grid[row, col].getName() == grid[row, col + 1].getName() && grid[row, col].getName() == grid[row, col + 2].getName())
+                    if (grid[row, col].getType() == grid[row, col + 1].getType() && grid[row, col].getType() == grid[row, col + 2].getType())
                     {
                         grid[row, col] = null;
                         grid[row, col + 1] = null;
                         grid[row, col + 2] = null;
                         candyDestroyed = true;
                     }
-                    if (row <= 7 && candyDestroyed == false)
-                    {
-                        if(grid[row, col].getName() == grid[row + 1, col].getName() && grid[row, col].getName() == grid[row + 2, col].getName())
-                        {
-                            grid[row, col] = null;
-                            grid[row + 1, col] = null;
-                            grid[row + 2, col] = null;
-                        }
-                    }
+                    //if (row <= 7 && candyDestroyed == false)
+                    //{
+                    //    if(grid[row, col].getType() == grid[row + 1, col].getType() && grid[row, col].getType() == grid[row + 2, col].getType())
+                    //    {
+                    //        grid[row, col] = null;
+                    //        grid[row + 1, col] = null;
+                    //        grid[row + 2, col] = null;
+                    //    }
+                    //}
                     if (candyDestroyed)
                     {
                         col += 2;
