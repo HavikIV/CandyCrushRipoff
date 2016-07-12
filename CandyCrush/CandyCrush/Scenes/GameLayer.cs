4ea1278e8df1c824701fe89bb0e1657bb1a52052
HavikIV <manjinder360@yahoo.com>
2016-07-11 18:10:14 -0700

    Removed the unnessecary libraries from candyLayer.cs, GameLayer.cs, GameScene.cs, TitleScene.cs, and GameController.cs files

--- CandyCrush/CandyCrush/Scenes/GameLayer.cs
+++ CandyCrush/CandyCrush/Scenes/GameLayer.cs
@@ -1,7 +1,5 @@
-using System;
 using System.Collections.Generic;
 using CocosSharp;
-using Microsoft.Xna.Framework;
 
 namespace CandyCrush
 {
--- CandyCrush/CandyCrush/Scenes/candyLayer.cs
+++ CandyCrush/CandyCrush/Scenes/candyLayer.cs
@@ -1,15 +1,6 @@
 using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
 using System.Threading.Tasks;
-
-using Android.App;
-using Android.Content;
-using Android.OS;
-using Android.Runtime;
-using Android.Views;
-using Android.Widget;
 
 using CocosSharp;
 using CandyCrush.Entities;
